9be3c6bd17c1957db9188fa93d6f03df99722a32
Scott <scottstephansandler@gmail.com>
2026-01-15 17:00:21 -0700

    working on navmesh

---
+++ Assets/Scipts/gameManager.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+    [SerializeField] TMP_Text gameGoalCountText;
+
+    public GameObject player;
+    public playerController playerScript;
+    public Image playerHPBar;
+    public GameObject playerDamageScreen;
+
+    public bool isPaused;
+
+    float timeScaleOrig;
+    int gameGoalCount;
+
+
+    sdf
+    // Start is called once before the first execution of Update after the MonoBehaviour is created dst
+    void Awake()
+    {
+        instance = this;
+
+        timeScaleOrig = Time.timeScale;
+
+        player = GameObject.FindWithTag("Player");
+       // playerScript = player.GetComponent<playerController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if(menuActive == null)
+            {
+                statePause();
+                menuActive = menuPause;
+                menuActive.SetActive(true);
+            }
+            else if (menuActive == menuPause)
+            {
+                StateUnpaused();
+            }
+        } 
+    }
+
+    public void statePause()
+    {
+        isPaused = true;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void StateUnpaused()
+    {
+        isPaused = false;
+        Time.timeScale = timeScaleOrig;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(false);
+        menuActive = null;
+    }
+
+    public void updateGameGoal(int amount)
+    {
+        gameGoalCount += amount;
+        gameGoalCountText.text = gameGoalCount.ToString("F0");
+        
+        if(gameGoalCount<= 0)
+        {
+            // you won!
+            statePause();
+            menuActive = menuWin;
+            menuActive.SetActive(true);
+        }
+    }
+
+    public void youLose()
+    {
+        statePause();
+        menuActive = menuLose;
+        menuActive.SetActive(true);
+    }
+}
